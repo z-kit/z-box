87d651e214cfb833c38d0b98ef1210ef06d4864f
Matheus Kautzmann <kautzmann5@gmail.com>
2017-02-06 13:59:55 -0200

    Speed up build process

--- test/regression/suite/component.js
+++ test/regression/suite/component.js
@@ -1,7 +1,7 @@
 module.exports = {
   default: (browser) => {
     browser
-      .url('http://localhost:3000/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
+      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
       .waitForElementPresent('.z-box', 1000)
       .assert.cssProperty('.z-box', 'padding', '16px')
       .assert.containsText('.z-box', 'Content')
