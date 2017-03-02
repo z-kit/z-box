7dc935e4f93dc17bdee0f23408dc89e1dfffe0c8
Matheus Kautzmann <kautzmann5@gmail.com>
2017-03-02 10:29:59 -0300

    Migrate nightwatch -> nightmare

--- test/regression/suite/css.js
+++ test/regression/suite/css.js
@@ -1,10 +1,26 @@
-module.exports = {
-  default: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=default')
-      .waitForElementPresent('.z-box', 1000)
-      .assert.cssProperty('.z-box', 'padding', '16px')
-      .assert.containsText('.z-box', 'Content')
-      .end();
-  },
-};
+import { test } from 'ava';
+import Nightmare from 'nightmare';
+
+test('CSS component - default', (t) => {
+  t.plan(1);
+  const msg = 'should render the box content';
+  const expected = 'Content';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=default')
+    .wait('.z-box')
+    .evaluate(() => document.querySelector('.z-box').textContent)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - default', (t) => {
+  t.plan(1);
+  const msg = 'should render a padded box';
+  const expected = '16px';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=default')
+    .wait('.z-box')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-box')).padding)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
